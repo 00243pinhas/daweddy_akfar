89cd9bb92b71cf533ae981a2cbec438374921d44
00243pinhas <pinaslok@gmail.com>
2025-08-26 22:26:53 +0300

    logo strugles

--- functions.php
+++ functions.php
@@ -1,10 +1,14 @@
 <?php
-// Enqueue parent theme stylesheet
 function afkar_child_enqueue_styles() {
     wp_enqueue_style('afkar-parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('afkar-child-style', get_stylesheet_directory_uri() . '/style.css', array('afkar-parent-style'));
 }
-add_action('wp_enqueue_scripts', 'afkar_child_enqueue_styles');
+add_filter('get_custom_logo', function($html) {
+    $custom_logo_url = get_stylesheet_directory_uri() . '/assets/img/logo.png';
+    return '<a href="' . esc_url(home_url('/')) . '" rel="home"><img src="' . esc_url($custom_logo_url) . '" alt="' . esc_attr(get_bloginfo('name')) . '" /></a>';
+});
 
-// You can add custom functions or modifications specific to the child theme below
+
 ?>
+
+
